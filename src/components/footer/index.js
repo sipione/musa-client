c5b5efe8d6f832780d671b7ad987aabd8b37ee57
Ricardo Deo Sipione Augusto <90251018+sipione@users.noreply.github.com>
2023-02-10 08:57:13 -0300

    social media banner

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -24,7 +24,7 @@ const FooterComponent = ()=>{
                 <FlexBoxContentCenter bg={bgPeople}/>
 
                 <FlexBoxContentCenter bg={bgSocial}>
-                    <TitleH3 className="flex-box__title">Acesse nossa comunidade no facebook</TitleH3>
+                    <TitleH3 className="flex-box__title">Acesse nossa comunidade no Telegram</TitleH3>
 
                     <a href={userLoged ? "https://t.me/+erXenQho3wYyZDdh" : "#/login"} target={userLoged ? "__blank" : "_self"} className="flex-box__button"><TitleH3 className="button__title">CLIQUE AQUI</TitleH3></a>
                 </FlexBoxContentCenter>
